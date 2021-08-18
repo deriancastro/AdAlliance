7e8862d00abb6f81b02508c814b26f5a4058154f
Derian Castro <deriancastro@hotmail.com>
2021-08-18 08:42:46 +0200

    Update routes v2

--- routes/ads.js
+++ routes/ads.js
@@ -2,8 +2,9 @@ const express = require('express');
 const database = require('../config/database');
 const app = express();
 
-app.get('/ads1', (req, res) => {
-    let sql = `SELECT * FROM table1`;
+app.get('/ads1/:hour', (req, res) => {
+    const{hour} = req.params;
+    let sql = `SELECT * FROM table1 WHERE hour = ${hour}`;
 
     database.query(sql, (err, result) => {
         if(err) {
@@ -21,8 +22,9 @@ app.get('/ads1', (req, res) => {
     });
 });
 
-app.get('/ads2', (req, res) => {
-    let sql = `SELECT * FROM table2`;
+app.get('/ads2/:hour', (req, res) => {
+    const{hour} = req.params;
+    let sql = `SELECT * FROM table2 WHERE hour = ${hour}`;
 
     database.query(sql, (err, result) => {
         if(err) {
