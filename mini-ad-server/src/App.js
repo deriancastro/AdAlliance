208d33d61a2218626e6e122013a3dfa8d508bb05
Derian Castro <deriancastro@hotmail.com>
2021-08-26 09:55:30 +0200

    Tooling

--- mini-ad-server/src/App.js
+++ mini-ad-server/src/App.js
@@ -62,7 +62,7 @@ export default function App() {
   }
 
   function toDetailsPage({currentLink}) {
-    setCurrentAdLink({currentLink});
+      setCurrentAdLink({ currentLink });
     push('/details');
   }
    
